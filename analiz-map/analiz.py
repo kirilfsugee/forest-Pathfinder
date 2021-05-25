3d3144bee657fa356e6b7db0acdecd95173e60aa
NovopashinVM <NovopashinVM@yandex.ru>
2021-05-25 18:37:28 +0300

    отображение карты

--- analiz-map/analiz.py
+++ analiz-map/analiz.py
@@ -1,1 +1,26 @@
-#test
+from kivy.app import App
+from kivy.uix.gridlayout import GridLayout
+from kivy.uix.label import Label
+from kivy.uix.image import Image
+
+
+class MainApp(App):
+    def build(self):
+        img = Image(source='../map_forest.png',
+                    size_hint=(1, 1),
+                    pos_hint={'center_x': .5, 'center_y': .5})
+
+        return img
+
+
+
+        # label = Label(text='Hello from Kivy',
+        #               size_hint=(.5, .5),
+        #               pos_hint={'center_x': .5, 'center_y': .5})
+        #
+        # return label
+
+
+if __name__ == '__main__':
+    app = MainApp()
+    app.run()
