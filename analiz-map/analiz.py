beefa1139176fce27ad0ef8a1eb21de9547f009b
NovopashinVM <NovopashinVM@yandex.ru>
2021-05-26 14:52:12 +0300

    добавил ресование связей

--- analiz-map/analiz.py
+++ analiz-map/analiz.py
@@ -4,7 +4,7 @@ from kivy.uix.label import Label
 from kivy.uix.image import Image
 from kivy.uix.button import Button
 from kivy.uix.boxlayout import BoxLayout
-from kivy.graphics import Rectangle, Color
+from kivy.graphics import Rectangle, Color, Line
 
 from kivy.config import Config
 
@@ -17,7 +17,7 @@ Config.set('input', 'mouse', 'mouse,disable_multitouch')
 Config.write()
 
 # списко точек
-list_point=[(470,249)]
+list_point=[(470,249),(594,287)]
 list_connection=[]
 # максимальное количество перекрестков
 max_count_points = 60
@@ -30,6 +30,8 @@ y_min = 0
 y_max = 995
 # режим нажатия кнопки мыши
 mouse_rejim = 0    # 0 ничего,  10 режим добавляения точки,  20 режим соединения
+# запомненная точка в 20 режиме
+temp_i_20 = -1
 
 class MainApp(App):
     def build(self):
@@ -62,6 +64,7 @@ class MainApp(App):
                                   padding=3,
                                   size_hint=( None, None),
                                   size= (190, 1000))
+        # отображаем список точек
         for i in range(count_points):
             self.layout_midle.add_widget(Label(text='point '+str(list_point[i])))
 
@@ -118,8 +121,10 @@ class MainApp(App):
         if mouse_rejim == 0: mouse_rejim = 10
 
     def add_connection(self, instance):
+        global mouse_rejim
         # функция экспорта кортежей
         if debug: print('Вы нажали на кнопку add_connection')
+        if mouse_rejim == 0: mouse_rejim = 20
 
     def delete(self, instance):
         # функция экспорта кортежей
@@ -127,7 +132,8 @@ class MainApp(App):
 
     # обработчик нажатия кнопки мыши
     def on_touch_up(self, touch,p):
-        global mouse_rejim
+        global mouse_rejim, temp_i_20
+        dopusk = 5
         x,y = p.pos
         x = int(x) - x_min
         y = y_max - int(y)
@@ -147,6 +153,32 @@ class MainApp(App):
                                           size=(10,10))
                 mouse_rejim = 0
 
+            elif mouse_rejim == 20:
+                #ищем есть ли рядом точка c x,y
+                if debug: print('точка in ' + str(x)+ ' y '+ str(y))
+                for i,(lx,ly) in enumerate(list_point):
+                    if debug: print('20 точка check ' + str(lx) + ' y ' + str(ly))
+                    if lx-dopusk < x < lx+dopusk and ly-dopusk < y < ly+dopusk:
+                        temp_i_20 = i
+                        mouse_rejim = 25
+                        if debug: print ('точка '+ str(temp_i_20) )
+                        break
+
+            elif mouse_rejim == 25:
+                #ищем есть ли рядом точка c x,y
+                if debug: print('25 точка in ' + str(x)+ ' y '+ str(y))
+                for i,(lx,ly) in enumerate(list_point):
+                    if debug: print('точка check ' + str(lx) + ' y ' + str(ly))
+                    if lx-dopusk < x < lx+dopusk and ly-dopusk < y < ly+dopusk:
+                        list_connection.append( (temp_i_20,i))
+                        mouse_rejim = 0
+                        if debug: print ('добавлено соединение между '+ str(temp_i_20) + ' и '+ str(i) )
+                        x1, y1 = list_point[i]
+                        x2, y2 = list_point[temp_i_20]
+                        with self.layout_main.canvas:
+                            Color(0, 0, 1, 1) # rgba
+                            Line(points=[x_min + x1, y_max - y1, x_min + x2, y_max - y2], width=2)
+                        break
 
 
 if __name__ == '__main__':
