ef74b453fd5266ab8e8809b794809cd43412791b
NovopashinVM <NovopashinVM@yandex.ru>
2021-05-26 23:34:21 +0300

    добавил удаление линии и отображение в одной функции

--- analiz-map/analiz.py
+++ analiz-map/analiz.py
@@ -43,32 +43,20 @@ class MainApp(App):
                 )
 
     def build(self):
-        #Clock.schedule_interval(self.update, 1)
+
         # создаем основную раскладку
         self.layout_main = BoxLayout(padding=3)  # Отступ padding между лейаутом
 
+        # создаем левую раскладку
         self.layout_left = BoxLayout(padding=0)
-        # добавлялем изображение
-        # img = Image(source='../map_forest.png',
-        #             # size_hint=(1, 1),
-        #             # pos_hint={'center_x': .5, 'center_y': .5},
-        #             # pos=(100, 110)
-        #             )
-        self.update(self)
-
         self.layout_main.add_widget(self.layout_left)
 
         # создаем среднюю раскладку для отображения точек
-        count_points = len(list_point)
         self.layout_midle = GridLayout(cols=1,
                                        rows=max_count_points,
                                        padding=3,
                                        size_hint=(None, None),
                                        size=(190, 1000))
-        # отображаем список точек
-        for i in range(count_points):
-            self.layout_midle.add_widget(Label(text='point ' + str(list_point[i])))
-
         self.layout_main.add_widget(self.layout_midle)
 
         # создаем среднюю раскладку для отображения соединений
@@ -77,13 +65,11 @@ class MainApp(App):
                                         padding=3,
                                         size_hint=(None, None),
                                         size=(190, 1000))
-        # отображаем список точек
-        for i in range(len(list_connection)):
-            self.layout_midle2.add_widget(Label(text='con ' + str(list_connection[i])))
-
         self.layout_main.add_widget(self.layout_midle2)
+        # отрисовываем всё
+        self.update(self)
 
-        # создаем правую раскладку
+        # создаем правую раскладку с кнопками
         layout_right = BoxLayout(padding=3,
                                  orientation="vertical",
                                  size_hint=(None, None),
@@ -153,22 +139,13 @@ class MainApp(App):
         x = int(x) - x_min
         y = y_max - int(y)
         if 0 < x < x_max and y_min < y < y_max and max_count_points > len(list_point) and mouse_rejim != 0:
-
-            if debug: print("coords x=" + str(x) + ' y=' + str(y))
+            if debug:
+                print("coords x=" + str(x) + ' y=' + str(y))
 
             if mouse_rejim == 10:  # добавляем точку
                 list_point.append((x, y))
-
-                lb = Label(text='point (' + str(x) + ',' + str(y) + ')')
-                self.layout_midle.add_widget(lb)
-
-                # with self.layout_main.canvas:
-                #     Color(1, 0, 1, 1)  #
-                #     rect = Rectangle(pos=(x_min + x - 5, y_max - y - 5),
-                #                           size=(10, 10))
                 self.update(self)
                 mouse_rejim = 0
-
             elif mouse_rejim == 20:  # ожидаем первую точку
                 # ищем есть ли рядом точка c x,y
                 if debug:
@@ -182,33 +159,25 @@ class MainApp(App):
                         if debug:
                             print('точка ' + str(temp_i_20))
                         break
-
             elif mouse_rejim == 25:  # ожидаем вторую точку
                 # ищем есть ли рядом точка c x,y
                 if debug:
                     print('25 точка in ' + str(x) + ' y ' + str(y))
                 for i, (lx, ly) in enumerate(list_point):
-                    if debug:
-                        print('точка check ' + str(lx) + ' y ' + str(ly))
+                    #if debug:
+                        #print('точка check ' + str(lx) + ' y ' + str(ly))
                     if lx - dopusk < x < lx + dopusk and ly - dopusk < y < ly + dopusk:
                         list_connection.append((temp_i_20, i))
                         mouse_rejim = 0
                         if debug:
                             print('добавлено соединение между ' + str(temp_i_20) + ' и ' + str(i))
-                        self.layout_midle2.add_widget(Label(text='con ' + str(temp_i_20) + ' и ' + str(i)))
-                        x1, y1 = list_point[i]
-                        x2, y2 = list_point[temp_i_20]
-                        with self.layout_main.canvas:
-                            Color(0, 0, 1, 1)  # rgba
-                            Line(points=[x_min + x1, y_max - y1, x_min + x2, y_max - y2], width=2)
+                        self.update(self)
                         break
             elif mouse_rejim == 30:  # удаление
                 # ищем есть ли рядом точка c x,y
                 if debug:
-                    print('25 точка in ' + str(x) + ' y ' + str(y))
+                    print('30 точка in x ' + str(x) + ' y ' + str(y))
                 for i, (lx, ly) in enumerate(list_point):
-                    if debug:
-                        print('20 точка check ' + str(lx) + ' y ' + str(ly))
                     if lx - dopusk < x < lx + dopusk and ly - dopusk < y < ly + dopusk:
                         # провеяоем нет ли её в соединениях
                         netu = True
@@ -223,19 +192,13 @@ class MainApp(App):
                 for i,(a, b) in enumerate(list_connection):
                     x1, y1 = list_point[a]
                     x2, y2 = list_point[b]
-                    print(abs((x - x1) * (y2 - y1) - (x2 - x1) * (y - y1)))
-                    if (x - x1) / (x2 - x1) == (y - y1) / (y2 - y1):
-                        print("yes")
-                    if abs((x - x1) * (y2 - y1) - (x2 - x1) * (y - y1)) < 5:
+#                    print (x, ' ', y,' ', x1,' ',x2,' ',y1,' ',y2)
+#                    print(abs((x - x1) * (y2 - y1) - (x2 - x1) * (y - y1)))
+                    #print("abs ", abs((x - x1) / (x2 - x1 + 0.00000000000001) - (y - y1) / (y2 - y1 + 0.000000000000001)))
+                    if abs((x - x1) / (x2 - x1+0.00001) - (y - y1) / (y2 - y1+0.00001))<0.46:
                         list_connection.pop(i)
                         self.update(self)
-
-
-
                 mouse_rejim = 0
-
-
-
 
 
     def update(self,p):
@@ -258,6 +221,15 @@ class MainApp(App):
             with self.layout_left.canvas:
                 Color(0, 0, 1, 1)  # rgba
                 Line(points=[x_min + x1, y_max - y1, x_min + x2, y_max - y2], width=2)
+
+        # отображаем список точек
+        self.layout_midle.clear_widgets()
+        for x, y in list_point:
+            self.layout_midle.add_widget(Label(text='point ' + str(x)+','+str(y)))
+
+        self.layout_midle2.clear_widgets()
+        for a, b in list_connection:
+            self.layout_midle2.add_widget(Label(text='con ' + str(a)+ ','+str(b)))
 
 
 if __name__ == '__main__':
